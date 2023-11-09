6a0da6158873182034803bba16ab11a8978debba
Robert Impey <robert-impey@users.noreply.github.com>
2023-11-09 23:39:08 +0000

    Reading a test file

--- 01_02/b/src/main.rs
+++ 01_02/b/src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use std::fs;
+
+fn main() {
+    let file_path = "test_file";
+    let contents: String = fs::read_to_string(file_path).expect("Unable to read file");
+    println!("{}", contents)
+}
