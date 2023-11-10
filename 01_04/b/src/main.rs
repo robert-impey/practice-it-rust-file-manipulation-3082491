ca8a9e623714f9e13724f5e559f16a70862355b4
Robert Impey <robert-impey@users.noreply.github.com>
2023-11-10 21:43:03 +0000

    Reading a file a line at a time

--- 01_04/b/src/main.rs
+++ 01_04/b/src/main.rs
@@ -1,3 +1,12 @@
+use std::io::{self, BufRead};
 use std::fs::File;
 
-fn main() {}
+fn main() {
+    let file_path = "file_with_lines";
+    let file: File = File::open(file_path).unwrap();
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line.unwrap());
+    }
+}
