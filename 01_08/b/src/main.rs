4bbace3a7ba286ee43b4c58f78b6f152ee4497f9
Robert Impey <robert-impey@users.noreply.github.com>
2023-11-12 16:37:41 +0000

    Writing to a file

--- 01_08/b/src/main.rs
+++ 01_08/b/src/main.rs
@@ -1,4 +1,12 @@
-fn main() {
+use std::fs::File;
+use std::io::Write;
+
+fn main() -> std::io::Result<()> {
     let file_path = "my_file";
     let contents = "This is my new file! Congrats!!!".to_string();
+
+    // https://doc.rust-lang.org/stable/std/io/trait.Write.html#method.write_all
+    let mut buffer = File::create(file_path)?;
+    buffer.write_all(contents.as_bytes())?;
+    Ok(())
 }
