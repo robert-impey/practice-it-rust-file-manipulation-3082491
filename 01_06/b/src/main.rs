799ac3121a3f3b308a89836788b24cbb25f9d57a
Robert Impey <robert-impey@users.noreply.github.com>
2023-11-12 16:18:47 +0000

    Filter lines in for loop

--- 01_06/b/src/main.rs
+++ 01_06/b/src/main.rs
@@ -1,3 +1,17 @@
+use std::io::{self, BufRead};
+use std::fs::File;
+
 fn main() {
     let wanted_string = "a";
+
+    let file_path = "file_with_lines";
+    let file: File = File::open(file_path).unwrap();
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let unwrapped = line.unwrap();
+        if unwrapped.contains(wanted_string) {
+            println!("{}", unwrapped);
+        }
+    }
 }
