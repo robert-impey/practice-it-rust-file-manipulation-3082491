7112ae7b3a931a4fba4b6e1ae5ac1b9274b09ab7
Robert Impey <robert-impey@users.noreply.github.com>
2023-11-15 22:54:45 +0000

    Function to print wanted lines

--- 02_02/b/src/main.rs
+++ 02_02/b/src/main.rs
@@ -1,7 +1,18 @@
+use std::fs;
+
 fn main() {
     let file_path = "file_with_lines";
     let wanted_string = "a";
 
-    // Create the below function
-    //print_wanted_lines_from_file(file_path, wanted_string);
+    print_wanted_lines_from_file(file_path, wanted_string);
+}
+
+fn print_wanted_lines_from_file(file_path: &str, wanted_string: &str) {
+    let contents: String = fs::read_to_string(file_path).expect("unable to open file");
+
+    for line in contents.lines() {
+        if line.contains(wanted_string) {
+            println!("{}", line);
+        }
+    }
 }
