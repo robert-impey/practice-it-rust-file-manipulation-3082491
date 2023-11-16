c068c434376a58f47b09ce01c803ebaf10d053e7
Robert Impey <robert-impey@users.noreply.github.com>
2023-11-16 23:03:37 +0000

    Read and return file in method

--- 02_04/b/src/main.rs
+++ 02_04/b/src/main.rs
@@ -1,7 +1,12 @@
+use std::fs;
+
+fn read_file(file_path: &str) -> String {
+    fs::read_to_string(file_path).expect("unable to open file")
+}
+
 fn main() {
     let file_path = "test_file";
 
-    // Implement the read_file function
-    //let contents = read_file(file_path);
-    //println!("{}", contents);
+    let contents = read_file(file_path);
+    println!("{}", contents);
 }
