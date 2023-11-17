d02c400010e8f5340c1638e1b64c33394d672bde
Robert Impey <robert-impey@users.noreply.github.com>
2023-11-17 16:05:50 +0000

    Read file and return a result

--- 02_06/b/src/main.rs
+++ 02_06/b/src/main.rs
@@ -1,7 +1,13 @@
+use std::fs;
+use std::io::Error;
+
+fn read_file(file_path: &str) -> Result<String, Error> {
+    fs::read_to_string(file_path)
+}
+
 fn main() {
     let file_path = "test_file";
     
-    // Implement the read_file function
-    //let contents = read_file(&file_path).expect(&format!("unable to read file <{}>", file_path));
-    //println!("{}", contents);
+    let contents = read_file(&file_path).expect(&format!("unable to read file <{}>", file_path));
+    println!("{}", contents);
 }
