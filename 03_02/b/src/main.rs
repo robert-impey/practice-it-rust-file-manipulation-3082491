645d0ddf6a6e0806c2958ae97788b5599af7af5f
Robert Impey <robert-impey@users.noreply.github.com>
2023-11-19 21:59:14 +0000

    Returning a vector of strings or error

--- 03_02/b/src/main.rs
+++ 03_02/b/src/main.rs
@@ -1,8 +1,19 @@
+use std::fs;
+
+fn read_file(file_path: &str) -> Result<Vec<String>, std::io::Error> {
+    let contents: String = fs::read_to_string(file_path)?;
+
+    let lines: Vec<String> = contents.lines()
+        .map(|l| l.to_string())
+        .collect();
+
+    Ok(lines)
+}
+
 fn main() {
     let file_path = "file_with_lines";
 
-    // Implement the read_file function
-    //let lines: Vec<String> = read_file(&file_path).expect(&format!("Error reading file <{}>", &file_path));
+    let lines: Vec<String> = read_file(&file_path).expect(&format!("Error reading file <{}>", &file_path));
 
-    //println!("{:#?}", lines);
+    println!("{:#?}", lines);
 }
