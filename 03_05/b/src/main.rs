e8d3d6cb8825568df45c2cc749112abf8659eb1a
Robert Impey <robert-impey@users.noreply.github.com>
2023-11-22 22:50:23 +0000

    Split lines into words

--- 03_05/b/src/main.rs
+++ 03_05/b/src/main.rs
@@ -1,8 +1,23 @@
+fn get_words(string: &String) -> Vec<String> {
+    let mut all_words: Vec<String> = Vec::new();
+
+    let lines: Vec<String> = string.split("\n")
+        .map(|s|s.to_string())
+        .collect();
+
+    for line in lines {
+        for word in line.split(" ") {
+            all_words.push(word.to_string());
+        }
+    }
+
+    return all_words;
+}
+
 fn main() {
     let contents = String::from("This is the first line\nThe second line is a little longer\nLine 3 is short\nThe 4th line is the first non-prime\nThe 5th line has the starting five");
 
-    // Implement the get_words function
-    //let words = get_words(&contents);
+    let words = get_words(&contents);
 
-    //println!("{:?}", words);
+    println!("{:?}", words);
 }
