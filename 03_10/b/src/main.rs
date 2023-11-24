0acf9958a95580eab42d63f5f90aa31c6f3e3486
Robert Impey <robert-impey@users.noreply.github.com>
2023-11-24 17:07:22 +0000

    Splitting a file to words

--- 03_10/b/src/main.rs
+++ 03_10/b/src/main.rs
@@ -1,8 +1,25 @@
+use std::fs;
+
+fn read_file(path: &str) -> Result<Vec<Vec<String>>, std::io::Error> {
+    let content = fs::read_to_string(path)?;
+
+    let lines: Vec<String> = content
+        .lines()
+        .map(|l|l.to_string())
+        .collect();
+
+    let words: Vec<Vec<String>> = lines
+        .iter()
+        .map(|l|l.split(" ").map(|w|w.to_string()).collect())
+        .collect();
+
+    Ok(words)
+}
+
 fn main() {
     let file_path = "file_with_lines";
 
-    // Implement the read_file function
-    //let lines = read_file(&file_path).expect(&format!("Unable to read file <{}>", &file_path));
+    let lines = read_file(&file_path).expect(&format!("Unable to read file <{}>", &file_path));
 
-    //println!("{:?}", lines);
+    println!("{:?}", lines);
 }
