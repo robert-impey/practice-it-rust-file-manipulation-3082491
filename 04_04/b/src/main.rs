f163a91beafbcef6cd2d248a5ae3943841e4136f
Robert Impey <robert-impey@users.noreply.github.com>
2023-11-29 23:05:50 +0000

    Replacing a word in a list

--- 04_04/b/src/main.rs
+++ 04_04/b/src/main.rs
@@ -1,5 +1,26 @@
 use std::collections::HashMap;
 
+fn get_words(text: &str) -> Vec<String> {
+    text.split_whitespace().map(|w|w.to_string()).collect()
+}
+
+fn replace_x_with_y_in_place(words: Vec<String>, replacement_map: &HashMap<String, String>) -> Vec<String> {
+    let mut updated_words: Vec<String> =  Vec::new();
+
+    for word in words {
+        let replacement = replacement_map.get(&word);
+
+        let updated_word = match replacement {
+            Some(rep) => rep.to_string(),
+            None => word,
+        };
+
+        updated_words.push(updated_word);
+    }
+
+    updated_words
+}
+
 fn main() {
     let contents = String::from("This is the first line\nThe second line is a little longer\nLine 3 is short\nThe 4th line is the first non-prime\nThe 5th line has the starting five");
     let replacement_map = HashMap::from([
@@ -7,9 +28,8 @@ fn main() {
         ("line".to_string(), "entry".to_string()),
     ]);
 
-    // Implement the get_words and replace_x_with_y_in_place functions
-    //let words = get_words(&contents);
-    //let new_words = replace_x_with_y_in_place(words.clone(), &replacement_map);
+    let words = get_words(&contents);
+    let new_words = replace_x_with_y_in_place(words.clone(), &replacement_map);
 
-    //println!("{:?}", new_words);
+    println!("{:?}", new_words);
 }
